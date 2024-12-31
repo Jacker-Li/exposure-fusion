c42404878d7f72babe171b64e08f52bbd558557a
Jacker-Li <166127456+Jacker-Li@users.noreply.github.com>
2024-12-31 22:02:47 +0800

    Update laplacian_pyramid.h

--- cpp_code/laplacian_pyramid.h
+++ cpp_code/laplacian_pyramid.h
@@ -1,8 +1,9 @@
 #ifndef EXPOSURE_FUSION_H
 #define EXPOSURE_FUSION_H
 
-#include <opencv2/opencv.hpp>
-#include <vector>
+#include<opencv2/opencv.hpp>
+#include<vector>
+
 using namespace cv;
 using namespace std;
 
