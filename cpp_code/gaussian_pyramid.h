7f5da80320bee75af668b695732d720d48a23d45
Jacker-Li <166127456+Jacker-Li@users.noreply.github.com>
2024-12-31 22:03:14 +0800

    Update gaussian_pyramid.h

--- cpp_code/gaussian_pyramid.h
+++ cpp_code/gaussian_pyramid.h
@@ -1,8 +1,8 @@
 #ifndef gaussian_pyramid_H
 #define gaussian_pyramid_H
 
-#include <opencv2/opencv.hpp>
-#include <vector>
+#include<opencv2/opencv.hpp>
+#include<vector>
 
 using namespace cv;
 using namespace std;
