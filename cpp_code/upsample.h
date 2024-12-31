7b815045c29f30e87850ce765e4d56f00db8f851
Jacker-Li <166127456+Jacker-Li@users.noreply.github.com>
2024-12-31 22:01:31 +0800

    Update and rename upsample.h to cpp_code/upsample.h

--- cpp_code/upsample.h
+++ cpp_code/upsample.h
@@ -1,9 +1,8 @@
 #ifndef upsample_H
 #define upsample_H
 
-#include <vector>
+#include<opencv2/opencv.hpp>
 
-using namespace std;
 using namespace cv;
 
 Mat upsample(const Mat& img,const Size& sz,const Mat& filter);
