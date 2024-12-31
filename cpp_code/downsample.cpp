e24436adb47cc8bec4eb3e19b9904c3bce1ac611
Jacker-Li <166127456+Jacker-Li@users.noreply.github.com>
2024-12-31 21:53:43 +0800

    Rename downsample.cpp to cpp_code/downsample.cpp

--- cpp_code/downsample.cpp
+++ cpp_code/downsample.cpp
@@ -3,7 +3,7 @@
 
 using namespace cv;
 
-//sepFilter2D£º°×Êép151
+//sepFilter2DÂ£ÂºÂ°Ã—ÃŠÃ©p151
 Mat downsample(const Mat& img,const Mat& filter){
     Mat tmp,res;
     sepFilter2D(img,tmp,CV_32F,filter,filter.t(),Point(-1,-1),0,BORDER_REFLECT);
